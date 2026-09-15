f6608ce3a17d28fac8abe2f4618f7848e6b71060
sam-010X <simran.bagga@cusec.in>
2026-09-15 09:39:32 +0530

    Use secrets for Supabase URL and key
    Replaced hardcoded Supabase credentials with secrets.

--- pages/3_Company_Details.py
+++ pages/3_Company_Details.py
@@ -149,8 +149,10 @@ st.set_page_config(
     layout="wide"
 )
 
-SUPABASE_URL = "https://lbiioyctwrwxwwbnaewa.supabase.co"
-SUPABASE_KEY = "sb_publishable_4ASf43j7RR3rE2c-DumiIg_4Tc5UvqH"
+
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
